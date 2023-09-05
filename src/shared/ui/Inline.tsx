b492196e3342eed1d372a7e6cc1e96816276462d
Adib Zidan <94Amsz@gmail.com>
2023-09-05 20:42:33 +0200

    Extend Inline component to render strong tags

--- src/shared/ui/Inline.tsx
+++ src/shared/ui/Inline.tsx
@@ -4,8 +4,17 @@ import './Inline.scss';
 interface Props {
     children: ReactNode;
     className?: string;
+    strong?: boolean;
 }
 
-export const Inline: FC<Props> = ({ children, className }) => (
-    <span className={className}>{children}</span>
-);
+export const Inline: FC<Props> = ({ children, className, strong = false }) => {
+    if (strong) {
+        return (
+            <strong>
+                <span className={className}>{children}</span>
+            </strong>
+        );
+    }
+
+    return <span className={className}>{children}</span>;
+};
