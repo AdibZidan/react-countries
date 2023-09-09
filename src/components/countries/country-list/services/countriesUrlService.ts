62074ee880354f8edb8a4cae2edb290008951e4b
Adib Zidan <94Amsz@gmail.com>
2023-09-09 18:45:00 +0200

    Correct BASE_URL

--- src/components/countries/country-list/services/countriesUrlService.ts
+++ src/components/countries/country-list/services/countriesUrlService.ts
@@ -1,4 +1,4 @@
 import { BASE_URL } from '../../services';
 
 export const composeCountryListUrl = (): string =>
-    `${BASE_URL}?fields=flags,name,population,region,capital`;
+    `${BASE_URL}/all?fields=flags,name,population,region,capital`;
--- src/components/countries/services/urlService.ts
+++ src/components/countries/services/urlService.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'https://restcountries.com/v3.1/all';
+export const BASE_URL = 'https://restcountries.com/v3.1';
