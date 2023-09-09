8aaab596d7f81bb9c1effda23e1eff2b7de31da1
Adib Zidan <94Amsz@gmail.com>
2023-09-09 18:43:16 +0200

    Extend Div ui component with optional onClick prop

--- src/shared/ui/Div.tsx
+++ src/shared/ui/Div.tsx
@@ -3,8 +3,11 @@ import { FC, ReactNode } from 'react';
 interface Props {
     children: ReactNode;
     className?: string;
+    onClick?: () => void;
 }
 
-export const Div: FC<Props> = ({ children, className }) => (
-    <div className={className}>{children}</div>
+export const Div: FC<Props> = ({ children, className, onClick = () => {} }) => (
+    <div className={className} onClick={onClick}>
+        {children}
+    </div>
 );
