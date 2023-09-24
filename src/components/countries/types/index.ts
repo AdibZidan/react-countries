30c93ce4c27f98f54708fc629192cbf6f0b05fc4
Adib Zidan <94Amsz@gmail.com>
2023-09-24 17:36:05 +0200

    Extend country types

--- src/components/countries/types/index.ts
+++ src/components/countries/types/index.ts
@@ -8,10 +8,38 @@ interface Name {
     common: string;
 }
 
+interface CountryDetailName {
+    common: string;
+    nativeName?: {
+        [key: string]: {
+            official: string;
+        };
+    };
+}
+
+type Currency = Record<string, { name: string }>;
+
+interface Languages {
+    [key: string]: string;
+}
+
 export interface Country {
     flags: Flags;
     name: Name;
     capital?: string[];
     region: string;
     population: number;
+}
+
+export interface CountryDetail {
+    flags: Flags;
+    name: CountryDetailName;
+    population: number;
+    region: string;
+    subregion?: string;
+    capital?: string[];
+    tld?: string[];
+    currencies?: Currency;
+    languages?: Languages;
+    borders?: string[];
 }
