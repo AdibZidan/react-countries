2b414fa3b7c4e147018050795f71febbc26c3d6d
Adib Zidan <94Amsz@gmail.com>
2023-11-04 18:49:08 +0100

    Configure react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.scss';
 
 const root = document.getElementById('root') as HTMLElement;
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: { refetchOnWindowFocus: false }
+    }
+});
 
 createRoot(root).render(
     <StrictMode>
         <BrowserRouter>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
         </BrowserRouter>
     </StrictMode>
 );
