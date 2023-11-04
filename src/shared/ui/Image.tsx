238ffb356bb3c845b9c406d0bcdf7f51451db502
Adib Zidan <94Amsz@gmail.com>
2023-11-04 18:42:48 +0100

    Extend Image with optional onLoad prop

--- src/shared/ui/Image.tsx
+++ src/shared/ui/Image.tsx
@@ -5,15 +5,23 @@ interface Props {
     src: string;
     alt: string;
     className?: string;
+    onLoad?: () => void;
 }
 
 export const Image: FC<Props> = ({
     'data-test': dataTest,
     src,
     alt,
-    className
+    className,
+    onLoad = () => {}
 }) => (
     <figure className={className}>
-        <img data-test={dataTest} loading="lazy" src={src} alt={alt} />
+        <img
+            data-test={dataTest}
+            loading="lazy"
+            src={src}
+            alt={alt}
+            onLoad={onLoad}
+        />
     </figure>
 );
