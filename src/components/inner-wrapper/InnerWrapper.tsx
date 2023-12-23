b24289053578b5f78f4accfcfc4958bdce4e00c3
Adib Zidan <94Amsz@gmail.com>
2023-12-23 14:22:58 +0100

    Extend InnerWrapper with optional baseClassName prop

--- src/components/inner-wrapper/InnerWrapper.tsx
+++ src/components/inner-wrapper/InnerWrapper.tsx
@@ -13,18 +13,24 @@ interface Props {
         | 'space-evenly'
         | 'center';
     wrap?: boolean;
+    baseClassName?: string;
 }
 
 export const InnerWrapper: FC<Props> = ({
     children,
     'data-test': dataTest,
     justifyContent = 'space-between',
-    wrap = false
+    wrap = false,
+    baseClassName
 }) => {
-    const className = classNames('inner-wrapper', {
-        [justifyContent]: justifyContent,
-        wrap
-    });
+    const className = classNames(
+        'inner-wrapper',
+        {
+            [justifyContent]: justifyContent,
+            wrap
+        },
+        baseClassName
+    );
 
     return (
         <Div data-test={dataTest} className={className}>
